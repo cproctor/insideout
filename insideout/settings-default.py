89441e0fd6f395382a0a19dcd99f8ddb91ab671a
Chris Proctor <chris.proctor@gmail.com>
2018-03-07 10:22:16 -0800

    Adding support for CORS headers

--- insideout/settings-default.py
+++ insideout/settings-default.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'insideout',
-    'feelings'
+    'feelings',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL= True
